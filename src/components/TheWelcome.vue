351544bd03be3cd1ca1ced76fe7334537acf1d61
Đorđe <xs55755@student.uni-lj.si>
2023-12-20 00:42:35 +0100

    X

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -85,4 +85,5 @@ import SupportIcon from './icons/IconSupport.vue'
     us by
     <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
   </WelcomeItem>
+  <p>Ovde cisto za proveru da sve lepo radi!</p>
 </template>
